9ede08deca1c920ba4546bae523764f5f945457d
Simon Søllingvraa <sso@danskecommodities.com>
2016-03-15 09:57:15 +0100

    Now uses jQuery for DOM manipulation instead of homebrewed functions

--- src/client/app/modules/toggle.js
+++ src/client/app/modules/toggle.js
@@ -1,4 +1,4 @@
-var myNP = myNP || {};
+var appModules = appModules || {};
 
 //apply revealing prototype pattern
 
@@ -9,7 +9,9 @@ var myNP = myNP || {};
      * on-click function for toggleable elements
      */
     $('.toggleable').each(function(index, element) {
-        element.onclick = toggle($('.' + element.id));
+        element.onclick = function() {
+            $('.' + element.id).toggleClass('-collapsed');
+        };
     });
     
     /**
@@ -17,41 +19,8 @@ var myNP = myNP || {};
      */
     $('#category-button')[0].onclick = function() {
         $('.categories').each(function(index, element) {
-            switchClass(element, 'hidden-xs', 'visible-xs');
-            switchClass(element, 'hidden-sm', 'visible-sm');
+            $(element).toggleClass('hidden-xs visible-xs');
+            $(element).toggleClass('hidden-sm visible-sm');
         });
     };
-
-    function toggle(elements) {
-        return function() {
-            $(elements).each(function(index, element) {
-                switchClass(element, '-collapsed', '-expanded');
-            });
-        };
-    }
-
-    function switchClass(element, first, second) {
-        if (hasClass(element, first)) {
-            replaceClass(element, first, second);
-        }
-        else if (hasClass(element, second)) {
-            replaceClass(element, second, first);
-        }
-    }
-
-    function addClass(element, className) {
-        element.className += ' ' + className;
-    }
-    
-    function removeClass(element, className) {
-        replaceClass(element, className, '');
-    }
-
-    function replaceClass(element, from, to) {
-        element.className = element.className.replace(from, to);
-    }
-
-    function hasClass(element, className) {
-        return element.className.indexOf(className) > -1;
-    }
 })();
