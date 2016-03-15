73697d3dd65f8d47e3af904b31f7d430c1f26d88
Simon Søllingvraa <sso@danskecommodities.com>
2016-03-15 14:32:26 +0100

    Added current state to selected navigation link

--- src/client/app/components/navigation.js
+++ src/client/app/components/navigation.js
@@ -2,15 +2,32 @@
 var appComponents = appComponents || {};
 
 appComponents.Navigation = function() {
-    
+
 };
 
 appComponents.Navigation.prototype = function() {
     return {
         setup: setup
     };
-    
+
     function setup() {
+        setupNavigationLinks();
+        setupNavigationButtons();
+    }
+    
+    function setupNavigationLinks() {
+        $('.navigation-link').each(function(index, element) {
+            element.onclick = function() {
+                $('.navigation-link').each(function(innerIndex, innerElement) {
+                    $(innerElement).removeClass('-current');
+                });
+                
+                $(this).addClass('-current');
+            };
+        });        
+    }
+    
+    function setupNavigationButtons() {
         $('#category-button')[0].onclick = function() {
             $(this).toggleClass('-pressed');
 
@@ -18,6 +35,6 @@ appComponents.Navigation.prototype = function() {
                 $(element).toggleClass('hidden-xs visible-xs');
                 $(element).toggleClass('hidden-sm visible-sm');
             });
-        };
+        };        
     }
-}();
+} ();
