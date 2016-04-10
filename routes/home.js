ab30aa8ba01642a0f963af4551ecab4ea3ce6293
Simon Søllingvraa <sso@danskecommodities.com>
2016-04-10 13:40:36 +0200

    The active nagivation class are now applied through node

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('home');
+router.get('/', function(req, res, next) {
+    res.render('home', {
+        active: { home: true }
+    });
 });
 
 module.exports = router;
