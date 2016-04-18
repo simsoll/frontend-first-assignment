a33b71e0b22c28fcb8c500a968b8388af9bf1713
Simon Søllingvraa <sso@danskecommodities.com>
2016-04-18 19:22:20 +0200

    Added properties to order view

--- handlers/orders.js
+++ handlers/orders.js
@@ -1,13 +1,25 @@
 'use strict';
-var orderRepository = require('../repositories/orderRepository.js');
+var orderService = require('../services/orderService.js');
 
 module.exports.list = function(req, res) {
     var context = {
         active: { orders: true },
-        approvedOrders: orderRepository.getByStatus('approved'),
-        pendingOrders: orderRepository.getByStatus('pending'),
-        submittedOrders: orderRepository.getByStatus('submitted')
+        approvedOrders: orderService.getByStatus('approved'),
+        pendingOrders: orderService.getByStatus('pending'),
+        submittedOrders: orderService.getByStatus('submitted'),
+        helpers: {
+            isEqual: isEqual
+        }
     };
 
     res.render('orders', context);
 };
+
+function isEqual(a, b, options) {
+    if (a === b) {
+        return options.fn(this);
+    }
+    else {
+        return options.inverse(this);
+    }
+}
