009a73076526abdb3a719d90ae70b66f5da619ba
Simon Søllingvraa <sso@danskecommodities.com>
2016-04-24 08:37:52 +0200

    Fixing barcode scanner

--- public/app/components/camera.js
+++ public/app/components/camera.js
@@ -12,44 +12,49 @@ appComponents.Camera.prototype = function () {
 
     function initialize() {
         $('.barcode-scanner').on('change', '#barcode-scanner', function (event) {
-            // Get a reference to the taken picture or chosen file
-            var files = event.target.files;
+            var applyTo = $('.modal-barcode .product__image');//.attr('src', imgURL);
+                        
+            injectPicture(event, applyTo);
+            
+            $('.modal-overlay[data-modal-class=".modal-barcode"]').toggleClass('hide');
+            $('.modal-barcode').toggleClass('hide');
+        });
+    }
 
-            if (files && files.length > 0) {
-                var file = files[0];
+    function injectPicture(event, applyTo) {
+        // Get a reference to the taken picture or chosen file
+        var files = event.target.files;
 
+        if (files && files.length > 0) {
+            var file = files[0];
+
+            try {
+                // Create ObjectURL
+                var imgURL = window.URL.createObjectURL(file);
+
+                // Set img src to ObjectURL
+                applyTo.src = imgURL;
+
+                // Revoke ObjectURL
+                URL.revokeObjectURL(imgURL);
+            }
+            catch (e) {
                 try {
-                    // Create ObjectURL
-                    var imgURL = window.URL.createObjectURL(file);
-
-                    // Set img src to ObjectURL
-                    showPicture.src = imgURL;
-
-                    // Revoke ObjectURL
-                    URL.revokeObjectURL(imgURL);
+                    // Fallback if createObjectURL is not supported
+                    var fileReader = new FileReader();
+                    fileReader.onload = function (event) {
+                        applyTo.src = event.target.result;
+                    };
+                    fileReader.readAsDataURL(file);
                 }
                 catch (e) {
-                    try {
-                        // Fallback if createObjectURL is not supported
-                        var fileReader = new FileReader();
-                        fileReader.onload = function (event) {
-                            showPicture.src = event.target.result;
-                        };
-                        fileReader.readAsDataURL(file);
+                    //
+                    var error = document.querySelector("#error");
+                    if (error) {
+                        error.innerHTML = "Neither createObjectURL or FileReader are supported";
                     }
-                    catch (e) {
-                        //
-                        var error = document.querySelector("#error");
-                        if (error) {
-                            error.innerHTML = "Neither createObjectURL or FileReader are supported";
-                        }
-                    }
-                }
-                finally {
-                    $('.modal-overlay').addClass('show');
-                    $('.modal-barcode').addClass('show');
                 }
             }
-        });
+        }
     }
 } ();
